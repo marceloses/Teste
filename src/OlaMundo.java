e6675441d8c12fc4e29e184ab79a2af628770064
Marcelo Severo <git@github.com:marceloses/APICatalogo.git>
2023-09-21 23:02:01 -0300

    Primeira Versão

---
+++ src/OlaMundo.java
@@ -0,0 +1,10 @@
+public class OlaMundo {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Hello Corinthians!");
+        System.out.println("Hello São Paulo!");
+        System.out.println("Hello Santos!");
+        System.out.println("Hello Palmeiras!");
+    }
+}
